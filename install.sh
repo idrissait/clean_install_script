0756e17639d9bb986eab9a50ab836afe89f9c6e8
idrissait <idriss.ni@gmail.com>
2019-06-25 11:37:11 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,45 @@
+#CLI
+xcode-select --install
+
+#brew install
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew update && brew doctor
+
+#chrome install
+brew cast install google-chrome
+
+
+#terminal username
+
+# create
+~/.bashrc
+~/.zshrc
+
+nano .bash_profile
+#add
+#export PS1=" "
+
+#brew install
+
+
+#python
+brew install pyenv
+brew install python
+brew install python3
+brew cask install anaconda
+
+#r
+brew install r
+brew cast install rstudio
+
+#qgis
+brew tap osgeo/osgeo4mac
+brew cask install adoptopenjdk
+brew cask install xquartz
+brew install qgis
+
+#other
+brew cask install atom
+brew cask install sublime-text
+brew install git
+
